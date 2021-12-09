c7c07a07fbf6f04e155313856f8548bc501a01cb
Iron Man <84199729+anhdung1002@users.noreply.github.com>
2021-12-09 17:05:28 -0500

    Able to handle all kinds of inputs

--- src/CreateInput.java
+++ src/CreateInput.java
@@ -5,18 +5,50 @@ public class CreateInput {
     public static int [] Exec(FileReader inputFile) throws IOException {
         // Populate the array from the inputFile
         BufferedReader reader = new BufferedReader(inputFile);
-        String input = reader.readLine();
-        reader.close();
-        String inputArr [] = input.split("\s.\s*");
+    
+                    // Case 1
+                    // String input = reader.readLine();
+                    // // input = input.trim();
+                    // String inputArr [] = input.split("\s\s*");
+                    // int size = inputArr.length;
 
-        int size = inputArr.length;
-        int intArray [] = new int [size];
-        for(int i = 0; i < size; i++){
-            intArray[i] = Integer.parseInt(inputArr[i]);
-        }
-        return intArray;
+
+            String inputArr [] = new String [10000]; 
+            int i = 0;
+            while(reader.ready()){
+                inputArr[i] = reader.readLine();
+                i++;
+            }
+            reader.close();
+
+
+            if(i > 5){
+                int intArray [] = new int [i];
+                for(int k = 0; k < i; k++){
+                    intArray[k] = Integer.parseInt(inputArr[k]);
+                }
+                return intArray;
+
+            }
+            else{
+                String input = inputArr[0];
+                    input = input.trim();
+                    String tempArr [] = input.split("\s\s*");
+                    int size = tempArr.length;
+                int intArray [] = new int [size];
+                for(int k = 0; k < size; k++){
+                    intArray[k] = Integer.parseInt(tempArr[k]);
+                }
+                return intArray;
+            }
+
+
+            // inputArr = input.split("\n");
+            // size = inputArr.length;
+
+        // int inputArray [] = new int [size];
+        // for(int i = 0; i < size; i++){
+        //     inputArray[i] = Integer.parseInt(inputArr[i]);
+        // }
     }
-
-
-
 }
